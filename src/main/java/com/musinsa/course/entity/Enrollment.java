0c8225aa26b1354912eda69620ef503c9f1c9b30
sycuuui <102959791+sycuuui@users.noreply.github.com>
2026-07-29 13:50:28 +0900

    #1 feat: Enrollment entity 구현

---
+++ src/main/java/com/musinsa/course/entity/Enrollment.java
@@ -0,0 +1,30 @@
+package com.musinsa.course.entity;
+
+import com.musinsa.course.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "enrollment",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "request_id",
+                        columnNames = {"student", "course"}
+                )
+        }
+)
+public class Enrollment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, columnDefinition = "bigint")
+    private Long id;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+}
