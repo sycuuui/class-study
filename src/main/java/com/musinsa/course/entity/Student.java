8ccdeb596763515086c072867cd2eb7a90039df6
sycuuui <102959791+sycuuui@users.noreply.github.com>
2026-07-29 13:41:59 +0900

    #1 feat: Student entity 구현

---
+++ src/main/java/com/musinsa/course/entity/Student.java
@@ -0,0 +1,23 @@
+package com.musinsa.course.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, columnDefinition = "bigint")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int max_credits;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
