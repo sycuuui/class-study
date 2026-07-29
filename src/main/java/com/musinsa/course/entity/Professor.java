f665cb9fd361a0f88971b5bab56254d4534d4387
sycuuui <102959791+sycuuui@users.noreply.github.com>
2026-07-29 13:36:34 +0900

    #1 feat: Professor entity 구현

---
+++ src/main/java/com/musinsa/course/entity/Professor.java
@@ -0,0 +1,20 @@
+package com.musinsa.course.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "professor")
+public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, columnDefinition = "bigint")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
