20e411c57dc4f50da30050c0537c5d828ed3f2f9
sycuuui <102959791+sycuuui@users.noreply.github.com>
2026-07-29 14:25:31 +0900

    #1 feat: 강좌 관련 jpa 메서드 생성

--- src/main/java/com/musinsa/course/repository/EnrollmentRepository.java
+++ src/main/java/com/musinsa/course/repository/EnrollmentRepository.java
@@ -1,7 +1,14 @@
 package com.musinsa.course.repository;
 
+import com.musinsa.course.entity.Course;
 import com.musinsa.course.entity.Enrollment;
+import com.musinsa.course.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    long countByCourse(Course course);
+    boolean existsByStudentAndCourse(Student student, Course course);
+    List<Enrollment> findByStudent(Student student);
 }
