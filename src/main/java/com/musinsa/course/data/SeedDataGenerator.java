b2acbb6eecb7d4e8c4578098b3ee2ce843dc8639
sycuuui <102959791+sycuuui@users.noreply.github.com>
2026-07-30 13:54:05 +0900

    #1 feat: DB 생성 순서 보장을 위해 @Order 추가

--- src/main/java/com/musinsa/course/data/SeedDataGenerator.java
+++ src/main/java/com/musinsa/course/data/SeedDataGenerator.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.Random;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Component
+@Order(1)
 public class SeedDataGenerator implements ApplicationRunner {
     private static final Logger log = LoggerFactory.getLogger(SeedDataGenerator.class);
     private static final int DEPARTMENT_MIN = 10;
